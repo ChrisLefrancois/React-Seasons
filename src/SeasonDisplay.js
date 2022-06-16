e4574422e187e4197e5619d4282429db8ec34246
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2022-06-16 14:49:48 -0400

    finished the ui design

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+  summer: {
+    text: 'let\' hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'
+  }
+}
 
 const getSeason = (lat, month) => {
   // check if we are in the norther or summer hemisphere, note we only have 2 season, summer nad winter
@@ -12,10 +24,14 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   // get the month and our lat to see if we are summer or winter
   const season = getSeason(props.lat, new Date().getMonth())
+  const { text, iconName } = seasonConfig[season];
+
 
   return (
-    <div>
-      Season Display!
+    <div className={ `season-display ${season}` }>
+      <i className={`${iconName} icon massive icon-left`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon massive icon-right`} />
     </div>
   );
 };
